a0f104f2b66c57f885307dbddaa94e148fef15f3
Paul Rus <paul.rus@liveedge.tech>
2025-04-25 10:36:18 +0300

    add prettier and eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'wt-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ng-workshop-tenaris';
